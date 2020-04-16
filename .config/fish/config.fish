51340c4d9b34d0deda78aa4a13022eef198155b8
Yuya Kodama <50483168+nixir@users.noreply.github.com>
2020-04-16 18:06:11 +0900

    Add googledrive-ocamlfuse alias on fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ alias ls exa
 
 alias ll 'ls -al'
 
+alias googledrive 'google-drive-ocamlfuse ~/GoogleDrive/'
+
 #https://nyakuo.hatenablog.com/entry/2019/07/01/233220
 if test -z $TMUX
   tmux new-session
