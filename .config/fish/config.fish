5dd6a696ca847d9af19ead8e61301849e32fae54
Yuya Kodama <ykodama@ieee.org>
2020-08-30 11:09:38 +0900

    add i3status-rust

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 
 
-alias grep rg
+#alias grep rg
 alias cat bat
 alias ls exa
 
@@ -8,7 +8,27 @@ alias ll 'ls -al'
 
 alias googledrive 'google-drive-ocamlfuse ~/GoogleDrive/'
 
+alias mailer 'firefox -P Mailer'
+alias zotero-firefox 'firefox -P Zotero'
+alias WOW 'firefox -P WOW'
+alias orico 'firefox -P Orico'
+
+
 #https://nyakuo.hatenablog.com/entry/2019/07/01/233220
 if test -z $TMUX
   tmux new-session
+end
+
+set PATH /home/nijuiti/.local/MATLAB/R2020a/bin $PATH
+set PATH /home/nijuiti/.local/bin $PATH
+
+set PATH /home/nijuiti/node_modules/.bin:$PATH
+#set npm_config_prefix /home/nijuiti/.node_modules
+
+
+set GPG_TTY tty
+
+
+function matlab --wraps matlab --description 'overload LD_PATH'
+	env LD_PRELOAD=/usr/lib/libstdc++.so env LD_LIBRARY_PATH=/usr/lib/xorg/modules/dri/ matlab
 end
